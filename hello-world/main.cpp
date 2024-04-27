edd1490e31d2983e1c52fdba33df050ab3cb6b59
Aleksandr Dubrovin <dubrovinmail@gmail.com>
2024-04-27 20:45:44 +0300

    add hairsegmentation to main.cpp

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -492,6 +492,13 @@ class GraphContainer {
           output_stream: "SEGMENTATION_MASK:segmentation_mask"
         }
 
+//        node {
+//          calculator: "HairSegmentationGpu"
+////          input_side_packet: "MODEL_SELECTION:model_selection"
+//          input_stream: "IMAGE:throttled_input_video"
+//          output_stream: "SEGMENTATION_MASK:segmentation_mask"
+//        }
+
         #node {
         #  calculator: "RecolorCalculator"
         #  input_stream: "IMAGE_GPU:throttled_input_video"
